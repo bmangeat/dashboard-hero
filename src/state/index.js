81ba079e6ff465ee3a92df0f355ff1a0c36111c0
bmangeat <brice.mangeat@gmail.com>
2020-04-14 15:41:41 +0200

    feat(state): setup object in state app regarding to heroapi and weather

---
+++ src/state/index.js
@@ -0,0 +1,42 @@
+export default {
+    weather: '',                                // "main" : Mist, Clear, Drizzle, Clouds, Rain, Fog, Dust, Haze, Snow, Thunderstorm
+    hero: {
+        name: '',
+        powerstats: {
+            intelligence: 0,
+            strengh: 0,
+            speed: 0,
+            durability: 0,
+            power: 0,
+            combat: 0
+        },
+        biography: {
+            fullName: '',
+            alterEgos: '',
+            aliases: [],
+            placeOfBirth: '',
+            firstAppearance: '',
+            publisher: '',
+            alignment: ''
+        },
+        appearance: {
+            gender: '',
+            race: '',
+            height: [],
+            weight: [],
+            eyeColor: '',
+            hairColor: ''
+        },
+        work: {
+            occupation: '',
+            base: ''
+        },
+        connection: {
+            groupAffiliation: '',
+            relatives: ''
+        },
+        image: {
+            url: ''
+        }
+    }
+}
