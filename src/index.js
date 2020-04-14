3bdeba989dbe989e67d02eaf9b533b6dfab06797
bmangeat <brice.mangeat@gmail.com>
2020-04-14 15:44:33 +0200

    refractor(css): add main scss + css calls in indexjs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import {app} from 'hyperapp'
 import { withLogger } from "@hyperapp/logger"
 
+import '../styles/*.css'
+import '../styles/*.scss'
+
 import actions from './actions'
 import state from './state'
 import view from './components/views/DashboardView'
 
-withLogger(app)(state, actions, view, document.body)
+withLogger(app)(state, actions, view, document.body);
