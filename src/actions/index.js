548980abac52b562e512018bf662c5d40dfe55c0
bmangeat <brice.mangeat@gmail.com>
2020-04-14 17:56:09 +0200

    feat(api): get data from weather api and store data in state is working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,54 @@
 import axios from 'axios'
 
-const API_HERO = 'https://superheroapi.com/api.php/10213784867090388/';
-const API_WEATHER = 'api.openweathermap.org/data/2.5/weather?id=5128581&appid=57a5dad53fd98ea8812e586a2b18d67e';
-
+const API_HERO = 'https://superheroapi.com/api.php/10213784867090388/'                                                      // url superheroapi with key : 10213784867090388
+const API_WEATHER = 'http://api.openweathermap.org/data/2.5/weather?id=5128581&appid=57a5dad53fd98ea8812e586a2b18d67e'      // url openweathermap with key : 57a5dad53fd98ea8812e586a2b18d67e
+                                                                                                                            //                    and idcity (NYC) : 5128581
 export default {
+
+    /**
+     * @desc Allow to set the state with the correct value
+     * @param object Object from API corresponding to the hero
+     */
     setHero: (object) => (state) => {
         return {...state, hero: object}
     },
+
+    /**
+     * @desc Get datas from API 'superheroapi'
+     */
     fetchHero: (id) => (state, actions) => {
-        const request = axios.get(API_HERO + id);
+        const request = axios.get(API_HERO + id)
         request.then(response => {
             actions.setHero(response.data)
         });
         return state
+    },
+
+    /**
+     * @desc Allow to set the state with the correct value
+     * @param item String from API corresponding to the main weather
+     */
+    setWeather: (item) => (state) =>{
+        return {...state, weather: item}
+    },
+
+    /**
+     * @desc Get datas from API 'openweathermap'
+     */
+    fetchWeather: () => (state, actions) => {
+        const request = axios.get(API_WEATHER)
+        request.then(response => {
+            actions.setWeather(response.data.weather[0].main)
+        })
+        return state
+    },
+
+    /**
+     * @desc Allow to setup state files. Call functions which call API in order to get data
+     * @param idHero Corresponding to one hero from API : get all datas
+     */
+    getData: (idHero) => (state, actions) => {
+        actions.fetchWeather()
+        actions.fetchHero(idHero)
     }
 }
--- src/components/views/DashboardView.js
+++ src/components/views/DashboardView.js
@@ -1,7 +1,7 @@
 import { h } from 'hyperapp'
 import Header from '../header/HeaderBar'
 
-export default (state, actions) => h('div',{ oncreate: () => actions.fetchHero(10)},[
+export default (state, actions) => h('div',{ oncreate: () => actions.getData(10) },[
     Header({
         hero: state.hero
     })
