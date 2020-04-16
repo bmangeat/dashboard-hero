04f7001cada3b9405bdf5793dedbb0f2ea55f06c
bmangeat <brice.mangeat@gmail.com>
2020-04-16 18:30:39 +0200

    refractor(selectHero): add other choice

--- src/components/header/SelectHero.js
+++ src/components/header/SelectHero.js
@@ -2,8 +2,12 @@ import {h} from 'hyperapp'
 
 export default (props) =>
     h('select', {id: 'list-hero', onchange: props.selectHero}, [
+        h('option', {}, ['Batman']),
         h('option', {}, ['Superman']),
-        h('option', {}, ['Batman'])
+        h('option', {}, ['Joker']),
+        h('option', {}, ['Catwoman']),
+        h('option', {}, ['Jack-Jack']),
+        h('option', {}, ['Harley Quinn']),
     ])
 
 
