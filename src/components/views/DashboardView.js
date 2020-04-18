663ef27352121db2461ec0cf1fc067e9b7bcd90e
Andrea <gllt.andrea@gmail.com>
2020-04-18 14:14:05 +0200

    feat(weather): Add css to component

--- src/components/views/DashboardView.js
+++ src/components/views/DashboardView.js
@@ -3,15 +3,17 @@ import Header from '../header/HeaderBar'
 import Weather from '../weather/Weather'
 
 export default (state, actions) => h('div',
-    {class: state.hero.name.replace(/ .*/, ''), oncreate: () => actions.selectHero()},
+{class: 'container-fluid ' + state.hero.name.replace(/ .*/, ''), oncreate: () => actions.selectHero()},
     [
         Header({
             hero: state.hero,
             selectHero: actions.selectHero
         }),
-        Weather({
-            weather: state.weather,
-            hero: state.hero
-        })
+        h('div', {class: 'row'}, [
+            Weather({
+                weather: state.weather,
+                hero: state.hero
+            })
+        ])
     ])
 
