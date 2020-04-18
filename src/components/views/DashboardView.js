174a1a2fc67838bf6126e25b1d383b30d0e63520
bmangeat <brice.mangeat@gmail.com>
2020-04-18 10:53:54 +0200

    feat(header): create branch + change props

--- src/components/views/DashboardView.js
+++ src/components/views/DashboardView.js
@@ -5,7 +5,8 @@ export default (state, actions) => h('div',
     {class: state.hero.name.replace(/ .*/, ''), oncreate: () => actions.selectHero()},
     [
         Header({
-            hero: state.hero,
+            name: state.hero.name,
+            photo: state.hero.image.url,
             selectHero: actions.selectHero
         })
     ])
