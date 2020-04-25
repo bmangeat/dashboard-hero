c2c141cb5cb366ba7bf8d81e9387eda3315f4448
Baptiste Ory <orybaptiste@gmail.com>
2020-04-25 22:42:15 +0200

    refractor(css) : clean and bugfixes

--- src/components/weather/WeatherMessage.js
+++ src/components/weather/WeatherMessage.js
@@ -6,9 +6,9 @@ export default (props) => {
     if (props.weather === 'Clear') {
         return <p>Life is good in <City hero={props.hero} /> : no villains on the horizon !</p>
     } else if (props.weather === 'Clouds' || props.weather === 'Dust' || props.weather === 'Haze') {
-        return <p>Birds sing in <City hero={props.hero} /> nd the villains hide !</p>
+        return <p>Birds sing in <City hero={props.hero} /> and the villains hide !</p>
     } else if(props.weather === 'Mist' || props.weather === 'Drizzle' || props.weather === 'Fog') {
-        return <p>Villains are discreet in <City hero={props.hero} />...</p>
+        return <p>The villains are discreet in <City hero={props.hero} />...</p>
     } else if(props.weather === 'Rain') {
         return <p>The villains are out on the streets of <City hero={props.hero} /> !</p>
     } else {
